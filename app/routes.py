36dc6ae97cb0d7c8066fddf548bcadec662e5598
Edith <you@example.com>
2018-09-07 15:45:51 +0300

    updated routes file

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Edith'}
-    return '''
-<html>
-    <head>
-        <H1><title>ONE MINUTE PITCH</title></H1>
-    </head>
-    <body>
-        <h1>Welcome, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    return render_template('index.html', title='Home', user=user)
