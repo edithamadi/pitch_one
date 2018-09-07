7ebca7bb19195a655e05a53d21f1121dc7492246
Edith <you@example.com>
2018-09-07 15:36:10 +0300

    displayed variable username in index file

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,13 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "One Minute Pitch"
+    user = {'username': 'Edith'}
+    return '''
+<html>
+    <head>
+        <H1><title>ONE MINUTE PITCH</title></H1>
+    </head>
+    <body>
+        <h1>Welcome, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
