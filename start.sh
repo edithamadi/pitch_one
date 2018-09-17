e0ac5fd0a0bb1c9005d7297c415aeee8d9018f1b
Edith <you@example.com>
2018-09-17 14:58:56 +0300

    deploy to heroku

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 
 export SECRET_KEY='JASMINE5e'
-export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://edith:JASMINE5e@localhost/pitch'
+export DATABASE_URL='postgres://etkstpttstatjq:abe22421ba475fb47d58a377cf6d2871889d4ca7e3f398232a90e10143c15adc@ec2-54-83-27-165.compute-1.amazonaws.com:5432/dflo4bn2t62u24'
 ​
 export MAIL_USERNAME='edithmcamadi@gmail.com'
 export MAIL_PASSWORD='JASMINE5e'
