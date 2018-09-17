8896e2483f65177a73287c06a87e483ec3b46608
Edith <you@example.com>
2018-09-17 12:11:39 +0300

    updated views model

--- app/main/views.py
+++ app/main/views.py
@@ -2,11 +2,9 @@ from flask import render_template,request,redirect,url_for,abort,flash
 from . import main
 from flask_login import login_required,current_user
 from ..models import User,Pitch,Comment,Upvote,Downvote
-from .forms import UpdateProfile,PitchForm
+from .forms import UpdateProfile,PitchForm,CommentForm
 from .. import db,photos
-
-
-
+# import markdown2
 
 # Views
 @main.route('/')
@@ -16,26 +14,110 @@ def index():
     View root page function that returns the index page and its data
     '''
     title = 'Pitch Application'
-    return render_template('index.html',title = title, Pitch = Pitch)
+    pitch = Pitch.query.all()
+    # categories = Category.get_categories()
+    return render_template('index.html',title = title, Pitch = pitch)
 
-
-@main.route('/pitch/new')
+@main.route('/pitch/new', methods=['GET','POST'])
 @login_required
 def new_pitch():
     form=PitchForm()
     if form.validate_on_submit():
-        pitches=Pitch(category=form.category.data,content=form.content.data)
+        pitches=Pitch(category=form.category.data,pitch_content=form.content.data)
         db.session.add(pitches)
         db.session.commit()
 
         flash('pitch created')
 
-        return redirect(url_for('main.new_pitch'))
     pitches=Pitch.query.all()
     return render_template('pitch.html',form=form, pitch=pitches)
 
-        
+#categories
 
+@main.route('/pickuplines/pitch')
+def pick_up():
+    pitch = Pitch.get_all_pitches()
+    title = 'PICKUP LINES'
+    return render_template('pickuplines.html',title = title,pitch = pitch)
+
+@main.route('/promotion/pitch')
+def promotion():
+    pitch = Pitch.get_all_pitches()
+    title = 'PROMOTION PITCH'
+    return render_template('promotion.html',title = title,pitch = pitch)
+
+@main.route('/technology/pitch')
+def technology():
+    pitch = Pitch.get_all_pitches()
+    title = 'TECHNOLOGY PITCH'
+    return render_template('technology.html',title = title,pitch = pitch)
+
+
+@main.route('/business/pitch')
+def business():
+    pitch = Pitch.get_all_pitches()
+    title = 'BUSINESS PITCH'
+    return render_template('business.html',title = title,pitch = pitch)
+
+#end of categories section
+
+# @main.route('/pitch/new/', methods = ['GET','POST'])
+# @login_required
+# def new_pitcJh():
+
+#     form = PitchForm()
+#     if category is None:
+#         abort( 404 )
+
+#     if form.validate_on_submit():
+#         pitch= form.content.data, form.category_id.data
+#         new_pitch= Pitch(pitch= pitch)
+    
+
+#         new_pitch.save_pitch()
+#         return redirect(url_for('main.index'))
+
+#     return render_template('npitch.html', npitch_form= form, category= category)
+
+
+@main.route('/category/<int:id>')
+def category(id):
+
+    category = PitchCategory.query.get(id)
+    category_name = PitchCategory.query.get(category_name)
+
+    if category is None:
+        abort(404)
+
+    pitch_in_category = Pitch.get_pitch(id)
+    return render_template('category.html' ,category= category, pitch= pitch_in_category)
+
+
+@main.route('/pitch/comments/new/<int:id>',methods = ['GET','POST'])
+@login_required
+def new_comment(id):
+    form = CommentForm()
+    if form.validate_on_submit():
+        new_comment = Comment(pitch_id =id,data=form.comment.data)
+        new_comment.save_comment()
+        return redirect(url_for('main.new_pitch'))
+    return render_template('ncomment.html', form=form)
+
+@main.route('/comments/<int:id>')
+def single_comment(id):
+    comment=Comment.query.get(id)
+    if comment is None:
+        abort(404)
+    return render_template('new_comment.html')
+
+@main.route('/view/comment/<int:id>')
+def view_comments(id):
+    '''
+    Function that shows the comments of a particular pitch
+    '''
+    comments = Comment.get_comments(id)
+    
+    return render_template('viewcomment.html',comments = comments, id=id)
 
 @main.route('/user/<uname>')
 def profile(uname):
